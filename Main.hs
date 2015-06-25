6dab49507a6d76c444b5033650f9eef077bc0c6f
mtsch <matijacufar@gmail.com>
2015-06-25 13:45:00 +0200

    error fixes, added the option of using different players

--- Main.hs
+++ Main.hs
@@ -39,14 +39,26 @@ readTwoInts s | length wds /= 2 = Nothing
                 where
                   wds = words s
 
--- readPair of ints from string
-readPair :: String -> Maybe (Int, Int)
-readPair s = readTwoInts s >>= listToMove
-
 -- turn list with two elements to pair
 listToMove :: [a] -> Maybe (a, a)
 listToMove [i1, i2] = Just (i1, i2)
 listToMove _        = Nothing
+
+-- readPair of ints from string
+readIntPair :: String -> Maybe (Int, Int)
+readIntPair s = readTwoInts s >>= listToMove
+
+-- read positive int
+readPositiveInt :: String -> Maybe Int
+readPositiveInt s = readMaybeInt s >>= (\ i -> if i > 0 
+                                               then Just i 
+                                               else Nothing)
+
+-- read valid size of board - board must be at least 2×2
+readSize :: String -> Maybe Size
+readSize s = readIntPair s >>= (\ (n, m) -> if n > 1 && m > 1
+                                              then Just (n, m)
+                                              else Nothing)
 
 -- try to get something that can be read by readFun from stdin
 getSomething :: (String -> Maybe a) -> String -> IO a
@@ -61,30 +73,44 @@ getSomething readFun errmsg = do
 
 -- get pair of Int from stdin
 getIntPair :: IO (Int, Int)
-getIntPair = getSomething readPair "Not a pair of numbers!"
+getIntPair = getSomething readIntPair "Not a pair of numbers!"
 
 -- get Int from stdin
 getInt :: IO Int
-getInt  = getSomething readMaybeInt "Not an integer!"
+getInt = getSomething readMaybeInt "Not an integer!"
 
--- attempt to put piece on board, report errors and retry if needed
-putPieceAndReport :: Player -> Board -> IO Board
-putPieceAndReport p b = do
-    move <- getIntPair
-    case putPiece p move b of
+-- get Int from stdin
+getSize :: IO Size
+getSize = getSomething readSize "Not a valid size! Must be at least 2×2!"
+
+-- positive int only
+getPositiveInt :: IO Int
+getPositiveInt = getSomething readPositiveInt "Not a valid positive integer!"
+
+-- ask human player for input
+humanPlayer :: GameState -> IO Move
+humanPlayer _ = getIntPair
+
+-- perform move made by player function
+performMoveBy :: (GameState -> IO Move) -> GameState -> IO Board
+performMoveBy player gs = do
+    move <- player gs
+    case putPiece (head $ queue gs) move (board gs) of
       Left err -> 
-        putStrLn ("Invalid input: " ++ err) >> putPieceAndReport p b
+        putStrLn ("Invalid input: " ++ err) >> performMoveBy player gs
       Right newBoard ->
         return newBoard
 
--- main loop
-loop :: GameState -> IO ()
-loop gs = do
+-- main loop - pair of players, playerX and playerO
+loop :: ((GameState -> IO Move), (GameState -> IO Move)) -> 
+        GameState -> IO ()
+loop players gs = do
     putStrLn $ showState gs
-    let b = board gs
-    let p = head $ queue gs
+    let currentPlayer = head $ queue gs
+    newBoard <- if currentPlayer == X
+                  then performMoveBy (fst players) gs
+                  else performMoveBy (snd players) gs
     let k = winK gs
-    newBoard <- putPieceAndReport p b
     case boardResult k newBoard of
       Winner w -> do putStrLn spacer
                      putStrLn $ (show w) ++ " wins!"
@@ -95,14 +121,15 @@ loop gs = do
       NotOver  ->
         let newState = GameState 
                          newBoard (winK gs)
-                         (tail (queue gs) ++ [head (queue gs)])
+                         (tail (queue gs) ++ [currentPlayer])
                          ((roundNo gs) + 1)
         in
-        loop newState
+        loop players newState
 
+-- choose type of game and begin
 main :: IO ()
 main = do putStrLn "Input board size:"
-          boardSize <- getIntPair
+          boardSize <- getSize
           putStrLn "Input winning k:"
-          winningK <- getInt
-          loop $ initialState boardSize winningK
+          winningK <- getPositiveInt
+          loop (humanPlayer, humanPlayer) $ initialState boardSize winningK
